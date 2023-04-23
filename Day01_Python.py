2344ea3cf1e9c1c7a669a5c92547ab483056761f
DamianCortex <DamianCortex3D@gmail.com>
2023-04-23 01:34:08 -0400

    git commit -m "Added new file: Day01_Python.py"

---
+++ Day01_Python.py
@@ -0,0 +1,12 @@
+# Prints Welcome Message
+print("Welcome to Band Name Generator")
+
+# Creates string variable based on user input on the city they grew up in.
+cityname = input("What's the name of the city you grew up in? ")
+
+# Creates string variable based on user input on their pet's name.
+petname = input("What's your pet's name? ")
+
+# Prints both variables with a space in between, generating the user's band name.
+print(cityname + ' ' + petname)
+
